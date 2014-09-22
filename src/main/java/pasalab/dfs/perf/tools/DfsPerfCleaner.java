a2313d38410618c99e5be9153228dab845e4652d
dongqianhao <09122SWAT@gmail.com>
2014-09-22 15:42:53 +0800

    format

--- src/main/java/pasalab/dfs/perf/tools/DfsPerfCleaner.java
+++ src/main/java/pasalab/dfs/perf/tools/DfsPerfCleaner.java
@@ -13,8 +13,10 @@ public class DfsPerfCleaner {
       fs.close();
     } catch (IOException e) {
       e.printStackTrace();
-      System.err.println("Failed to clean workspace " + PerfConf.get().DFS_DIR);
+      System.err.println("Failed to clean workspace " + PerfConf.get().DFS_DIR + " on "
+          + PerfConf.get().DFS_ADDRESS);
     }
-    System.out.println("Clean the workspace " + PerfConf.get().DFS_DIR);
+    System.out.println("Clean the workspace " + PerfConf.get().DFS_DIR + " on "
+        + PerfConf.get().DFS_ADDRESS);
   }
 }
