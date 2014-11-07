c0e7a854e2ce8b0ef3ea33d06e5ca84335a656b0
dongqianhao <09122SWAT@gmail.com>
2014-11-07 19:01:40 +0800

    accelerate for multi-process test

--- bin/dfs-perf-start.sh
+++ bin/dfs-perf-start.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 function printUsage {
-  echo "Usage: dfs-perf-start.sh <NodeName> <TaskId> <TestCase>"
+  echo "Usage: dfs-perf-start.sh <NodeName> <TaskIdFrom> <TaskIdTo> <TestCase>"
   echo "This is used to start dfs-perf on each node, see more in ./dfs-perf"
 }
 
-# if less than 3 args specified, show usage
-if [ $# -le 2 ]; then
+# if less than 4 args specified, show usage
+if [ $# -le 3 ]; then
   printUsage
   exit 1
 fi
@@ -24,7 +24,9 @@ fi
 
 JAVACOMMAND="$JAVA -cp $DFS_PERF_CONF_DIR/:$DFS_PERF_JAR -Dpasalab.dfs.perf.home=$DFS_PERF_HOME -Dpasalab.dfs.perf.logger.type=PERF_SLAVE_LOGGER -Dlog4j.configuration=file:$DFS_PERF_CONF_DIR/log4j.properties $DFS_PERF_JAVA_OPTS pasalab.dfs.perf.DfsPerfSlave"
 
-echo "Starting dfs-perf task-$2 @ `hostname -f`"
-(nohup $JAVACOMMAND $* > /dev/null 2>&1 ) &
-
+for (( i = $2; i <= $3; i ++))
+do
+  echo "Starting dfs-perf task-$i @ `hostname -f`"
+  (nohup $JAVACOMMAND $1 $i $4 > /dev/null 2>&1 ) &
+done
 sleep 1
